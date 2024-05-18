af5b2232de065adab308c1e4c40e500384878cd7
fukuimaunicorn <the.bros.85.03@gmail.com>
2024-05-18 09:17:41 +0200

    create server + implement session

---
+++ apps/server/connectors/redis.ts
@@ -0,0 +1,15 @@
+import session from "express-session";
+import * as redis from "redis";
+import ConnectRedis from "connect-redis";
+
+export const redisClient = redis.createClient({
+  legacyMode: true,
+  url: process.env.REDIS_URL,
+});
+redisClient.connect().catch((err) => console.error(err));
+
+export const redisStore = new ConnectRedis({
+  client: redisClient,
+  prefix: "pp3s-session:",
+  ttl: 360000,
+});
---
+++ apps/server/middleware/session.ts
@@ -0,0 +1,32 @@
+import session from "express-session";
+import { v4 as uuid } from "uuid";
+import { redisStore } from "../connectors/";
+
+console.log(process.env.NODE_ENV);
+
+const sessionOptions = {
+  secret: process.env?.SESSION_SECRET || "secret",
+  genid: () => {
+    return uuid();
+  },
+  name: "arena-web-session",
+  cookie: {
+    httpOnly: true,
+    secure: false,
+    sameSite: false,
+    maxAge: 1000 * 60 * 60 * 24,
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          domain: process.env.COOKIE_BASE,
+          // TODO: Set secure to true when we have HTTPS
+          secure: false,
+        }
+      : {}),
+  },
+  resave: true,
+  saveUninitialized: true,
+  store: redisStore,
+} as const;
+
+const sessionMiddleware = session(sessionOptions);
+export default sessionMiddleware;
