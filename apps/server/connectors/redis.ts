53808b2056fcde305559725fe31533e475dfe44e
fukuimaunicorn <the.bros.85.03@gmail.com>
2024-06-28 15:11:06 +0200

    try using ioredis instead

--- apps/server/connectors/redis.ts
+++ apps/server/connectors/redis.ts
@@ -1,10 +1,21 @@
 import * as redis from "redis";
 import ConnectRedis from "connect-redis";
+import { Redis } from "ioredis";
 
-export const redisClient = redis.createClient({
-  url: process.env.REDIS_URL,
+// export const redisClient = redis.createClient({
+//   url: process.env.REDIS_URL,
+// });
+const redisClient = new Redis(process.env.REDIS_URL);
+redisClient.on("connect", () => {
+  console.log("Redis connected");
 });
-redisClient.connect().catch((err) => console.error(err));
+redisClient.on("error", (err) => {
+  console.error("Redis error", err);
+});
+redisClient.on("reconnecting", () => {
+  console.log("Redis reconnecting");
+});
+//redisClient.connect().catch((err) => console.error(err));
 
 export const redisStore = new ConnectRedis({
   client: redisClient,
