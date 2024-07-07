56513ebbe3a0f3db3f9ba254bed2d99f9ed21374
imaron85 <the.bros.85.03@gmail.com>
2024-07-07 14:54:47 +0200

    working up to join game

--- apps/web/app/join/page.tsx
+++ apps/web/app/join/page.tsx
@@ -1,9 +1,25 @@
 "use client";
 
-import { Button } from "@ui/button";
-import { InputOTP, InputOTPGroup, InputOTPSlot } from "@ui/input-otp";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Join() {
+  const [code, setCode] = useState("");
+
+  const validateCode = (code: string) => {
+    // regex that replaces all non-numeric characters with empty string
+    const regex = /[^\d]/g;
+    let formattedCode = code.replace(regex, "");
+
+    // insert a space every 3 digits
+    formattedCode =
+      formattedCode.length > 3
+        ? (formattedCode.match(/.{1,3}/g)?.join(" ") as string)
+        : formattedCode;
+
+    setCode(formattedCode);
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-background">
       <div className="max-w-2xl w-full px-6 sm:px-8 lg:px-12">
@@ -16,21 +32,23 @@ export default function Join() {
           </p>
         </div>
         <div className="mt-12">
-          <InputOTP maxLength={6}>
-            <InputOTPGroup>
-              <InputOTPSlot index={0} />
-              <InputOTPSlot index={1} />
-              <InputOTPSlot index={2} />
-              <InputOTPSlot index={3} />
-              <InputOTPSlot index={4} />
-              <InputOTPSlot index={5} />
-            </InputOTPGroup>
-          </InputOTP>
+          <input
+            type="text"
+            className="mt-1 text-9xl block w-full px-3 py-2 border border-gray-300 text-center rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            placeholder="123 456"
+            maxLength={7}
+            accept="numeric"
+            value={code}
+            onChange={(e) => validateCode(e.target.value)}
+          />
         </div>
-        <div className="mt-8">
-          <Button className="w-full bg-primary text-primary-foreground hover:bg-primary/90 p-2 rounded-xl">
+        <div className="mt-8 w-full flex justify-center">
+          <Link
+            href={"/game/" + code?.replace(/ /g, "")}
+            className="w-full bg-primary text-primary-foreground text-center hover:bg-primary/90 p-2 rounded-xl"
+          >
             Join
-          </Button>
+          </Link>
         </div>
       </div>
     </div>
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,24 +5,24 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen bg-background">
       <div className="space-y-6 text-center">
         <h1 className="text-6xl font-bold tracking-tighter">Game Night</h1>
-        <p className="text-muted-foreground text-xl">Create a new game or join an existing one.</p>
+        <p className="text-muted-foreground text-xl">
+          Create a new game or join an existing one.
+        </p>
       </div>
       <div className="flex flex-col gap-4 mt-12">
         <Link
           href="/create"
           className="inline-flex items-center justify-center w-full h-14 px-12 text-lg rounded-md bg-neutral-200 text-black shadow transition-colors hover:bg-black/30 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-          prefetch={false}
         >
           Create Game
         </Link>
         <Link
           href="/join"
           className="inline-flex items-center justify-center w-full h-14 px-12 text-lg rounded-md bg-black text-white shadow transition-colors hover:bg-black/80 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-          prefetch={true}
         >
           Join via Code
         </Link>
       </div>
     </div>
-  )
+  );
 }
