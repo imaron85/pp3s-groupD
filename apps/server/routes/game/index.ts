b5d2e054a15ce43662c17c76e42291332d30b076
imaron85 <the.bros.85.03@gmail.com>
2024-07-08 21:41:28 +0200

    breaks stuff

--- apps/server/routes/game/index.ts
+++ apps/server/routes/game/index.ts
@@ -1,10 +1,3 @@
 import { Router } from "express";
-import { get } from "./get";
 
 export const gameRouter = Router();
-
-gameRouter.get("/", (req, res) => {
-  console.log("Received request to /game");
-});
-
-gameRouter.get("/:code", get);
